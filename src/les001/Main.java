d585ba4cbac235223e6503354f12ba8b7f85ba39
CrocodileDaniil <karasev_dan_an@mail.ru>
2025-11-03 20:10:32 +0300

    add les 001

---
+++ src/les001/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String name = "Daniil";
+		int x;
+		x = 25;
+		int y = 19;
+		
+		System.out.println("Hello, Eclipse " + name + " your age: "+ x);
+		
+		x = x + y - (y = x);
+		
+		System.out.println("x = " + x + " y = " + y);
+		
+	}
+
+}
